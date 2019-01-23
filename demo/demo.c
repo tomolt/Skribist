02768eb0a2a0ba09f7d3dbd9b622af92e9305a1b
Thomas Oltmann <thomas.oltmann.hhg@gmail.com>
2019-01-23 16:43:19 +0100

    Splitting lines by pixel boundaries.

--- demo/demo.c
+++ demo/demo.c
@@ -3,49 +3,54 @@
 #include <stdint.h>
 #include <math.h>
 
-#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define min(a, b) ((a) < (b) ? (a) : (b))
+#define max(a, b) ((a) > (b) ? (a) : (b))
 
 #define WIDTH 256
 #define HEIGHT 256
-#define ANGLES 10
+
+typedef struct {
+	double x, y;
+} point_t;
+
+typedef struct {
+	point_t pts[2];
+} line_t;
 
 static uint8_t image[WIDTH * HEIGHT];
 
-static void bresenham(int x0, int y0, int x1, int y1)
+static void split_line(line_t line)
 {
-	int bx = MIN(x0, x1); // , by = MIN(y0, y1);
-	int lx = abs(x1 - x0), ly = abs(y1 - y0);
-	if (lx >= ly) {
-		if (x0 > x1) {
-			int xt = x0, yt = y0;
-			x0 = x1, y0 = y1;
-			x1 = xt, y1 = yt;
+	double sx = line.pts[0].x;
+	double sy = line.pts[0].y;
+	double dx = line.pts[1].x - line.pts[0].x;
+	double dy = line.pts[1].y - line.pts[0].y;
+
+	double minx = min(line.pts[0].x, line.pts[1].x);
+	double miny = min(line.pts[0].y, line.pts[1].y);
+	double maxx = max(line.pts[0].x, line.pts[1].x);
+	double maxy = max(line.pts[0].y, line.pts[1].y);
+
+	double ts[1000];
+	int ts_top = 0;
+
+	if (dx != 0.0) {
+		for (int wx = ceil(minx); wx <= floor(maxx); ++wx) {
+			double t = (wx - sx) / dx;
+			ts[ts_top++] = t;
 		}
-		int es = y0 <= y1 ? 1 : -1; // error sign
-		int eu = 2 * lx; // error unit
-		int dy = 2 * ly;
-		int y = y0;
-		int ye = eu / 2; // error along y
-		for (int x = bx; x <= bx + lx; ++x) {
-			int c = 255 - ye * 255 / eu;
-			image[y * WIDTH + x] = c;
-			image[(y + es) * WIDTH + x] = 255 - c;
-			ye += dy;
-			y += es * (ye / eu);
-			ye = ye % eu;
+	}
+
+	if (dy != 0.0) {
+		for (int wy = ceil(miny); wy <= floor(maxy); ++wy) {
+			double t = (wy - sy) / dy;
+			ts[ts_top++] = t;
 		}
-	} else {
-		if (y0 > y1) {
-			int xt = x0, yt = y0;
-			x0 = x1, y0 = y1;
-			x1 = xt, y1 = yt;
-		}
-		double dx = (double) (x1 - x0) / (y1 - y0);
-		double x = x0 + 0.5;
-		for (int y = y0; y <= y1; ++y) {
-			image[y * WIDTH + (int) x] = 255;
-			x += dx;
-		}
+	}
+
+	for (int i = 0; i < ts_top; ++i) {
+		double t = ts[i];
+		printf("t = %f, f(t) = (%f, %f)\n", t, sx + t * dx, sy + t * dy);
 	}
 }
 
@@ -84,17 +89,8 @@ static void write_bmp(void)
 
 int main(int argc, char const *argv[])
 {
-	double mx = WIDTH / 2, my = HEIGHT / 2;
-	double ri = 20;
-	double ro = 0.5 * MIN(WIDTH, HEIGHT) - 20;
-	for (int i = 0; i < ANGLES; ++i) {
-		double angle = (2.0 * M_PI) * i / ANGLES;
-		double xd = sin(angle);
-		double yd = cos(angle);
-		bresenham(
-			(int) (mx + xd * ri), (int) (my + yd * ri),
-			(int) (mx + xd * ro), (int) (my + yd * ro));
-	}
-	write_bmp();
+	line_t line = { { { 5.5, 3.5 }, { 182.3, 101.1 } } };
+	split_line(line);
+	// write_bmp();
 	return EXIT_SUCCESS;
 }
