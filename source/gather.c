65d6af80968186cb8aa478dcc624cff687563f92
Thomas Oltmann <thomas.oltmann.hhg@gmail.com>
2019-02-22 18:08:28 +0100

    gather precision improvements.

--- source/gather.c
+++ source/gather.c
@@ -19,15 +19,15 @@ gamma correction and sub-pixel rendering (if enabled).
 void olt_INTERN_gather(void)
 {
 	for (int r = 0; r < HEIGHT; ++r) {
-		long acc = 0; // in the range -127 - 127
+		long acc = 0; // in the range -255 - 255
 		for (int c = 0; c < WIDTH; ++c) {
 			int i = WIDTH * r + c;
 			RasterCell *cell = &olt_GLOBAL_raster[i];
 			int windingAndCover = cell->windingAndCover, area = cell->area;
-			int value = acc + windingAndCover * area / 254; // in the range -127 - 127
-			int normValue = value * 255 / 127; // TODO we lose a lot of precision here.
-			olt_GLOBAL_image[i] = abs(normValue); // TODO use standardized winding direction to obviate the need for this abs()
-			acc += windingAndCover;
+			int upscaledCover = windingAndCover * 255 / 127;
+			int value = acc + upscaledCover * area / 254; // in the range -255 - 255
+			olt_GLOBAL_image[i] = abs(value); // TODO use standardized winding direction to obviate the need for this abs()
+			acc += upscaledCover;
 		}
 	}
 }
