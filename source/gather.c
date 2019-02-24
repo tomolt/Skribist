7f604c884ca5069087d524e712ff9f12ea386e66
Thomas Oltmann <thomas.oltmann.hhg@gmail.com>
2019-02-24 15:17:26 +0100

    fill streak glitch further realization.

--- source/gather.c
+++ source/gather.c
@@ -28,11 +28,13 @@ void olt_INTERN_gather(void)
 			assert(area >= 0 && area <= 254);
 			int upscaledCover = windingAndCover * 255 / 127;
 			int value = acc + upscaledCover * area / 254; // in the range -255 - 255
+			// FIXME value overflows by *1*, meaning it goes to 256, but not beyond.
 			// TODO the clamp probably shouldn't be neccessary at all
 			// TODO use standardized winding direction to obviate the need for this abs()
 			int clampedValue = clamp(abs(value), 0, 255);
 			olt_GLOBAL_image[i] = clampedValue;
 			acc += upscaledCover;
 		}
+		// assert(acc != 0);
 	}
 }
