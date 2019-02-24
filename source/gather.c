54bb461a5b980af558b22d413e6b3eaf11f420e3
Thomas Oltmann <thomas.oltmann.hhg@gmail.com>
2019-02-24 15:05:32 +0100

    Fixed internal fill streak glitch.

--- source/gather.c
+++ source/gather.c
@@ -24,9 +24,14 @@ void olt_INTERN_gather(void)
 			int i = WIDTH * r + c;
 			RasterCell *cell = &olt_GLOBAL_raster[i];
 			int windingAndCover = cell->windingAndCover, area = cell->area;
+			assert(windingAndCover >= -127 && windingAndCover <= 127);
+			assert(area >= 0 && area <= 254);
 			int upscaledCover = windingAndCover * 255 / 127;
 			int value = acc + upscaledCover * area / 254; // in the range -255 - 255
-			olt_GLOBAL_image[i] = abs(value); // TODO use standardized winding direction to obviate the need for this abs()
+			// TODO the clamp probably shouldn't be neccessary at all
+			// TODO use standardized winding direction to obviate the need for this abs()
+			int clampedValue = clamp(abs(value), 0, 255);
+			olt_GLOBAL_image[i] = clampedValue;
 			acc += upscaledCover;
 		}
 	}
