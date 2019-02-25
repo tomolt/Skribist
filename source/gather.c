ac65cf75b980f0799e6986187be39e33918f87d9
Thomas Oltmann <thomas.oltmann.hhg@gmail.com>
2019-02-25 14:57:11 +0100

    Finally fixed a big gather bug.

--- source/gather.c
+++ source/gather.c
@@ -21,18 +21,18 @@ gamma correction and sub-pixel rendering (if enabled).
 void olt_INTERN_gather(void)
 {
 	for (int r = 0; r < HEIGHT; ++r) {
-		long acc = 0; // in the range -255 - 255
+		long acc = 0; // in the range -127 - 127
 		for (int c = 0; c < WIDTH; ++c) {
 			int i = WIDTH * r + c;
 			RasterCell *cell = &olt_GLOBAL_raster[i];
 			int windingAndCover = cell->windingAndCover, area = cell->area;
 			assert(windingAndCover >= -127 && windingAndCover <= 127);
 			assert(area >= 0 && area <= 254);
-			int upscaledCover = windingAndCover * 255 / 127;
-			int value = acc + upscaledCover * area / 254; // in the range -255 - 255
+			int value = acc + windingAndCover * area / 254; // in the range -127 - 127
+			int scaledValue = value * 255 / 127; // in the range -255 - 255
 			// TODO use standardized winding direction to obviate the need for this abs()
-			olt_GLOBAL_image[i] = abs(value);
-			acc += upscaledCover;
+			olt_GLOBAL_image[i] = abs(scaledValue);
+			acc += windingAndCover;
 		}
 		printf("%ld\n", acc);
 	}
