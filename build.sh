51675f0e54f7a942db911a62d6ce6f36b39899b8
Thomas Oltmann <thomas.oltmann.hhg@gmail.com>
2019-04-22 00:58:04 +0200

    Less SSE stores in RasterizeDots().

--- build.sh
+++ build.sh
@@ -10,9 +10,9 @@ build_all() {
 	# examples
 	$CC $CFLAGS -g -O0 examples/bmp/bmp_example.c -o examples/bmp/bmp_example -Iinclude source/Skribist.c &
 	# stress tests
-	# $CC $CFLAGS     -O3 stress/stress.c -o stress/stress.op -Iinclude source/Skribist.c &
-	# $CC $CFLAGS -g  -O0 stress/stress.c -o stress/stress.dg -Iinclude source/Skribist.c &
-	# $CC $CFLAGS -pg -O0 stress/stress.c -o stress/stress.pg -Iinclude source/Skribist.c &
+	$CC $CFLAGS     -O3 stress/stress.c -o stress/stress.op -Iinclude source/Skribist.c &
+	$CC $CFLAGS -g  -O0 stress/stress.c -o stress/stress.dg -Iinclude source/Skribist.c &
+	$CC $CFLAGS -pg -O0 stress/stress.c -o stress/stress.pg -Iinclude source/Skribist.c &
 	wait
 }
 
