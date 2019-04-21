bfaef0a8cfd5e12f6990284e1ddea8a9f67d5dc4
Thomas Oltmann <thomas.oltmann.hhg@gmail.com>
2019-04-21 13:50:54 +0200

    build system improvements.

--- build.sh
+++ build.sh
@@ -1,30 +1,19 @@
 #!/bin/sh
 
-ENV_ERR="You have to set up a build environment first."
+ENV_ERR="You have to select a C compiler first."
 
 [ -z $CC ] && { echo $ENV_ERR; exit 1; }
 
-CFLAGS="$CFLAGS -std=gnu99 -pedantic -Wall -Wextra"
-
-build_Skribist() {
-	$CC $CFLAGS -c source/Skribist.c -o Skribist.o -Iinclude
-	ar -rcs libSkribist.a Skribist.o
-}
-
-build_examples() {
-	$CC $CFLAGS examples/bmp/bmp_example.c -o examples/bmp/bmp_example -Iinclude libSkribist.a -lm
-}
-
-build_stress() {
-	$CC -O3 stress/stress.c -o stress/stress.op -Iinclude libSkribist.a -lm
-	$CC -g -O0 stress/stress.c -o stress/stress.dg -Iinclude libSkribist.a -lm
-	$CC -pg -O3 stress/stress.c -o stress/stress.pg -Iinclude libSkribist.a -lm
-}
+CFLAGS="-std=gnu99 -pedantic -Wall -Wextra"
 
 build_all() {
-	build_Skribist
-	build_examples
-	build_stress
+	# examples
+	$CC $CFLAGS -g -O0 examples/bmp/bmp_example.c -o examples/bmp/bmp_example -Iinclude source/Skribist.c -lm &
+	# stress tests
+	$CC $CFLAGS     -O3 stress/stress.c -o stress/stress.op -Iinclude source/Skribist.c -lm &
+	$CC $CFLAGS -g  -O0 stress/stress.c -o stress/stress.dg -Iinclude source/Skribist.c -lm &
+	$CC $CFLAGS -pg -O0 stress/stress.c -o stress/stress.pg -Iinclude source/Skribist.c -lm &
+	wait
 }
 
 time build_all
